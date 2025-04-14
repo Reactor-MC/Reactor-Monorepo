1d44b01f3d94aabc9f701fbb8220adde822c9c3f
iChocoMilk <ichocodev@gmail.com>
2025-04-14 01:10:50 -0300

    change settings.gradle.kts to automatic load libraries and plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,18 @@
 rootProject.name = "reactor"
 
-val libs = arrayOf("util", "nbt", "chat", "item", "entity", "world", "zlib", "buffer", "command")
-for (lib in libs) {
-    include("libs:$lib")
-}
-
 include("api")
 include("protocol")
 include("server")
 include("data-parser")
 
-val plugins = arrayOf("debug", "atom-chat")
-for (plugin in plugins) {
-    include("plugins:$plugin")
+file("libs").listFiles()?.forEach { libDir ->
+    if (libDir.isDirectory) {
+        include("libs:${libDir.name}")
+    }
+}
+
+file("plugins").listFiles()?.forEach { pluginDir ->
+    if (pluginDir.isDirectory) {
+        include("plugins:${pluginDir.name}")
+    }
 }
